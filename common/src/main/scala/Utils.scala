0ac4f2d36c1c78c8dc16f68121e475c926619736
Doyoung Kim <d0319@postech.ac.kr>
2024-12-04 16:14:26 +0900

    Fix: Fixed the bug that repeat() method does not repeat properly.

--- common/src/main/scala/Utils.scala
+++ common/src/main/scala/Utils.scala
@@ -9,7 +9,7 @@ package object general {
 
       for (i <- 0 until x.size) {
         if (x(i) != y(i))
-          return (x(i).toShort & 0xFF) - (y(i).toShort & 0xFF)
+          return (x(i).toShort & 0xff) - (y(i).toShort & 0xff)
       }
 
       return 0
@@ -17,7 +17,7 @@ package object general {
   }
 
   implicit class ByteOps(byte: Byte) {
-    def toHexString: String = String.format("%02X", byte & 0xFF)
+    def toHexString: String = String.format("%02X", byte & 0xff)
   }
 
   implicit class ByteStringOps(bytes: com.google.protobuf.ByteString) {
@@ -148,7 +148,7 @@ package object concurrent {
 
     def repeat(callback: => Future[Boolean]): Future[Unit] =
       callback.flatMap {
-        case true => repeat(callback)
+        case true  => repeat(callback)
         case false => Future.unit
       }
   }
@@ -173,7 +173,7 @@ package object concurrent {
     import scala.util.{Success, Failure}
 
     def after[U](callback: T => U): Unit = future.onComplete {
-      case Success(value) => callback(value)
+      case Success(value)     => callback(value)
       case Failure(exception) => throw exception
     }
   }
