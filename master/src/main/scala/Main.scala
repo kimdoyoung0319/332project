d08842e0aca2fb6bd1c980f47e9ce156a8ecf188
Your Name <you@example.com>
2024-12-06 01:13:39 +0900

    Feat : Add function of auto execution(by call shell script)

--- master/src/main/scala/Main.scala
+++ master/src/main/scala/Main.scala
@@ -3,18 +3,23 @@ package master
 object Main {
   def main(args: Array[String]): Unit = {
     require(
-      args.size == 1,
-      "Only the number of workers should be given as an argument."
+      args.size == 2,
+      "Two arguments are required: [1] count: the number of workers, [2] size: {small, big, large}."
     )
     require(
-      args(0).toIntOption != None,
-      "The first argument should be the number of workers."
+      args(0).toIntOption.isDefined,
+      s"Invalid first argument '${args(0)}'. Please provide a valid integer for the number of workers."
+    )
+    require(
+      Set("small", "big", "large").contains(args(1)),
+      s"Invalid second argument '${args(1)}'. Please provide one of the following: small, big, large."
     )
 
     val count = args(0).toInt
+    val size = args(1)
     val server = new Server(count)
 
-    server.start()
+    server.start(size)
     server.await()
   }
 }
--- master/src/main/scala/Server.scala
+++ master/src/main/scala/Server.scala
@@ -2,16 +2,19 @@ package master
 
 class Server(count: Int) {
   import utils.concurrent.PromiseCompanionOps
+  import os._
 
   private val finished = scala.concurrent.Promise.withCallback(stop)
   private val logger = utils.logger.LoggerFactoryUtil.getLogger("master")
   private val service = new Service(count, finished)
   private val server = utils.grpc.makeServer(service.binded)
 
-  def start(): Unit = {
+  def start(size: String): Unit = {
     server.start()
+    val port = server.getPort()
     logger.info(s"Master server listening to ${server.getPort()} started.")
-    println(s"${utils.network.thisIp}:${server.getPort()}")
+    println(s"${utils.network.thisIp}:$port")
+    executeScript(port, size)
   }
 
   def await(): Unit = server.awaitTermination()
@@ -19,5 +22,17 @@ class Server(count: Int) {
   private def stop: Unit = {
     logger.info("Shutting down master server...")
     server.shutdown()
+  }
+
+  private def executeScript(port: Int, size: String): Unit = {
+    val scriptName = "deploy_worker.sh"
+    val scriptPath = os.Path(s"/home/blue/scripts/$scriptName")
+    try {
+      val result = os.proc(scriptPath, port.toString, size).call()
+      logger.info(s"Executed $scriptName with port $port: ${result.out.text().trim}")
+    } catch {
+      case e: os.SubprocessException =>
+        logger.error(s"Failed to execute $scriptName: ${e.getMessage}")
+    }
   }
 }
